a0d51b5ea5f2cc0ee75c9c073ff8fd84823c94b6
timkiely <timothy.j.kiely@gmail.com>
2018-01-18 17:21:07 -0500

    sales downloader now includes rolling sales 12 months

--- R/03-download-sales-data.R
+++ R/03-download-sales-data.R
@@ -3,8 +3,6 @@
 
 download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.rds") {
   
-  # TODO: paralellize? 
- 
   # create a dest directory if not already exists 
   if(!dir.exists("data/aux data/sales data raw")){
     dir.create("data/aux data/sales data raw")
@@ -23,7 +21,17 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
       discard(function(x) str_detect(x, "citywide|sales_statistics|sale_statistics|sales_prices"))
   } else stop("Rolling Sales URL not reachable: http://www1.nyc.gov/site/finance/taxes/property-annualized-sales-update.page")
   
-  yet_to_download <- links[!basename(links) %in% dir("data/aux data/sales data")]
+  # add 12 month rolling sales data:
+  if(url.exists("http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page")){
+    twelve_month <-
+      read_html("http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page") %>% 
+      html_nodes(".rolling_sales a") %>% html_attr('href') %>% 
+      map_chr(function(x) paste0("http://www1.nyc.gov",x)) %>% 
+      keep(function(x) str_detect(x, "[.]xls"))
+  } else stop("Rolling Sales URL not reachable: http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page")
+  
+  links <- c(links, twelve_month)
+  yet_to_download <- links[!basename(links) %in% dir("data/aux data/sales data raw")]
   
   # for testing:
   #yet_to_download <- yet_to_download[1:10]
@@ -36,7 +44,7 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
          , ignore.stdout = TRUE
          , ignore.stderr = TRUE
          , wait = TRUE)
-
+  
   message("     ...xls files downloaded")
   
   # clean up the cache index
@@ -47,11 +55,20 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
   for(j in 1:length(our_files)){ 
     
     file_name <- our_files[j]
-    file_name_csv <- str_replace(file_name, "xlsx|xls","csv")
     year <- 
       file_name %>% str_extract_all("[0-9]") %>%
       unlist() %>% paste0(collapse = "") %>% 
       map_dbl(function(x) ifelse(nchar(x)==2, as.numeric(paste0("20",x)),as.numeric(x)))
+    
+    # only re-download the rolling sales data once per month:
+    if(is.na(year)){
+      year <- "rolling"
+      file_name_csv <- str_replace(file_name, "xlsx|xls","csv")
+      file_name_csv <- paste0(lubridate::year(Sys.Date()),sprintf("%02.0f",lubridate::month(Sys.Date())), "_",file_name_csv)
+    } else {
+      file_name_csv <- str_replace(file_name, "xlsx|xls","csv")
+    }
+    
     
     message("Working on ", file_name)
     
@@ -59,9 +76,9 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
       message("      ....already downloaded, reading from disk")
       sales_df_out[[j]] <- 
         suppressWarnings(suppressMessages(
-        read_csv(paste0("data/aux data/sales data csv/",file_name_csv)
-                 , col_types = cols(.default = "c")
-                 , progress = FALSE)
+          read_csv(paste0("data/aux data/sales data csv/",file_name_csv)
+                   , col_types = cols(.default = "c")
+                   , progress = FALSE)
         ))
       message("      ....done")
     } else {
@@ -83,7 +100,7 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
   
   
   ## making column names consistent
-  
+  message("Final processing of data...")
   nms <- 
     names(sales_df_out[[1]]) %>% 
     str_replace("[.]$","") %>% 
@@ -91,27 +108,34 @@ download_nyc_sales <- function(save_file = "data/processing steps/p03_sales_raw.
   
   sales_df_out <- 
     sales_df_out %>% map(function(x) {
-    names(x) <- nms
-    x <- x %>% mutate_all(as.character)
-    x <- x %>% mutate_all(trimws)
-    return(x)
+      names(x) <- nms
+      x <- x %>% mutate_all(as.character)
+      x <- x %>% mutate_all(trimws)
+      return(x)
     }
     )
-
+  
   ## putting list into single dataframe
   final_object <- bind_rows(sales_df_out)
+  # de-dupe the list, in case rolling sales were included in past trasactions
+  final_object <- 
+    final_object %>% 
+    distinct(BOROUGH, BLOCK, LOT, `EASE MENT`
+             , ADDRESS, `SALE DATE`, `SALE PRICE` 
+             , `GROSS SQUARE FEET`, .keep_all = TRUE)
   
-  suppressWarnings({
-    final_object <-
-      final_object %>% 
-      mutate(
-        `BUILDING CLASS CATEGORY` = trimws(`BUILDING CLASS CATEGORY`)
-        , `ZIP CODE` = as.integer(`ZIP CODE`)
-        , `SALE PRICE` = as.numeric(`SALE PRICE`)
-        , `SALE DATE` = as.Date(`SALE DATE`, format = "%m/%d/%Y")
-      ) %>% 
-      mutate_at(vars(`RESIDENTIAL UNITS`:`YEAR BUILT` ), as.numeric)
-  })
+  final_object <-
+    final_object %>%
+    mutate(
+      `BUILDING CLASS CATEGORY` = trimws(`BUILDING CLASS CATEGORY`)
+      , `ZIP CODE` = as.integer(`ZIP CODE`)
+      , SALE_DATE = as.Date(`SALE DATE`, format = "%Y-%m-%d")
+    ) %>% 
+    mutate(`SALE PRICE` = as.numeric(str_replace_all(`SALE PRICE`, "[$]|[,]|[-]",""))) %>% 
+    mutate_at(vars(`RESIDENTIAL UNITS`:`GROSS SQUARE FEET`), str_replace_all, "[$]|[,]|[-]","") %>% 
+    mutate_at(vars(`RESIDENTIAL UNITS`:`YEAR BUILT` ), as.numeric) %>% 
+    mutate(`SALE YEAR` = lubridate::year(SALE_DATE))
+  
   message("Writing to disk...")
   write_rds(final_object, save_file)
   message("Complete sales data stored at ",save_file)

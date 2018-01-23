dff46c2b3ff3ea69c4f7eece64735300908ea916
timkiely <timothy.j.kiely@gmail.com>
2018-01-23 18:09:56 -0500

    moving on to base modeling data

--- R/06-create-base-data.R
+++ R/06-create-base-data.R
@@ -3,5 +3,13 @@
 create_base_data <- function(pluto_with_sales_infile = "data/processing steps/p05_pluto_with_sales.rds"
                              , outfile = "data/processing steps/p06_base_model_data.rds") {
   
+  pluto <- read_rds(pluto_with_sales_infile)
+  
+  # pluto %>% glimpse()
+  # 
+  # pluto %>% summary()
+  # 
+  # pluto %>% filter(Sold==1)
+  
   message("TODO: function to create the BASE modeling data")
 }

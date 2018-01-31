4fe74dc439c363037431ab8e5586bab88de26808
timkiely <timothy.j.kiely@gmail.com>
2018-01-31 03:17:07 +0000

    improved flow control from Rscript

--- R/00-script.R
+++ R/00-script.R
@@ -16,17 +16,23 @@ source("R/helper/source-files.R")
 
 # data --------------------------------------------------------------------
 # approx. 5 minutes
-if(tolower(DL) != "skip-dl"){
+if(is.na(DL)) DL <- "N"
+if(tolower(DL) == "skip-dl"){
+  message("=====> Bypassing download function")
+} else {
   download_nyc_pluto( save_file = "data/processing steps/p01_pluto_raw.rds")
   download_nyc_pad(   save_file = "data/processing steps/p02_pad_raw.rds") # steps 1 and 2 take 13.5 minutes
   download_nyc_sales( save_file = "data/processing steps/p03_sales_raw.rds") # steps 1, 2 and 3 take 55 mins (from scratch)
-  
-} else message("=====> Bypassing download function")
+}  
 
 
 # processing --------------------------------------------------------------
 # approx. 30 minutes
-if(tolower(PP) != "skip-pp"){
+if(is.na(PP)) PP <- "N"
+if(tolower(PP) == "skip-pp") {
+  message("=====> Bypassing preprocessing function")
+} else {
+  
   combine_sales_and_pad(sales_infile = "data/processing steps/p03_sales_raw.rds"
                         , pad_infile = "data/processing steps/p02_pad_raw.rds"
                         , pluto_infile = "data/processing steps/p01_pluto_raw.rds"
@@ -53,8 +59,7 @@ if(tolower(PP) != "skip-pp"){
   create_radii_data(pluto_with_sales_infile = "data/processing steps/p05_pluto_with_sales.rds"
                     , outfile = "data/processing steps/p08_radii_model_data.rds")
   
-} else message("=====> Bypassing preprocessing function")
-
+}
 
 # Prob of sale model ------------------------------------------------------
 
